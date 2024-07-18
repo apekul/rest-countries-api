e425ee7029b284cf04e7db06cd3639c3281ea010
Adaś <adampekul@gmail.com>
2024-07-18 20:45:48 +0200

    create basic project structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+// https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca
 function App() {
-  return <div className="bg-red-200">Content</div>;
+  return (
+    <section className="">
+      <Navbar />
+      <Footer />
+    </section>
+  );
 }
 
 export default App;
