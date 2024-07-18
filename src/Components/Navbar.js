271c4c6d322e0cab2c00dc44836127c473b0cf69
Adaś <adampekul@gmail.com>
2024-07-18 21:12:54 +0200

    install react icons, add icons for dark/light mode

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,16 @@
 import React from "react";
-
+import { IoMoonOutline, IoMoonSharp } from "react-icons/io5";
 const Navbar = () => {
   return (
     <navbar className="paddingX flex items-center justify-between py-6 bg-white shadow-md">
       <h1 className="text-2xl font-[800]">Where in the world?</h1>
       <ul>
         <li className="flex gap-2">
-          <div>img</div>
+          <IoMoonOutline />
           <p>Dark Mode</p>
         </li>
         <li className="hidden gap-2">
-          <div>img</div>
+          <IoMoonSharp />
           <p>Light Mode</p>
         </li>
       </ul>
